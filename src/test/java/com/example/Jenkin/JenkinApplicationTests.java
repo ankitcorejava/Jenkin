e1e77d15c156fe5e251871d5de66fa5543bb83e4
ankitcorejava <107574427+ankitcorejava@users.noreply.github.com>
2024-08-04 00:27:21 +0530

    TC

--- src/test/java/com/example/Jenkin/JenkinApplicationTests.java
+++ src/test/java/com/example/Jenkin/JenkinApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.Jenkin;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinApplicationTests {
 
+	Logger loggers = Logger.getAnonymousLogger();
+
 	@Test
 	void contextLoads() {
+
+		loggers.log(null, "Testing....");
 	}
 
 }
